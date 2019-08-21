13a288e58c89473de6da798596be26648883a922
rakeshg-coditas <rakesh.ghonmode@coditas.com>
2019-08-21 19:42:04 +0530

    First Commit

--- src/main/java/tavisca/interfaces/CacheFunctionalities.java
+++ src/main/java/tavisca/interfaces/CacheFunctionalities.java
@@ -1,15 +1,38 @@
 package tavisca.interfaces;
 
 public interface CacheFunctionalities {
-
+    /*
+    *        Add the object into cache.
+    *        Params: Key  - Unique identifier for the object.
+    *                       If same key is used for another object , existing object will be updated
+    *               value - object
+    *               periodInMillisSeconds  -  validity time of the object
+    * */
     public void add(String key, Object value, long periodInMilliSeconds);
 
+
+    /*
+    *       get the object from the cache using key.Key here is unique identifier for that object
+    *
+    * */
     public Object get(String key);
 
+    /*
+     *       remove the object from the cache using key.Key here is unique identifier for that object
+     *
+     * */
     public void remove(String key);
 
+    /*
+     *       Clears the whole cache
+     *
+     * */
     public void clear();
 
+    /*
+     *       get the size of the current object.It will be changed as per the time specified in Project Constants.
+     *
+     * */
     public long size();
 
 }
